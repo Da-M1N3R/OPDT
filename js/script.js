e2d7b00f606fd99d1ab150665f0e12c13bbc0b1f
Edgar <uranium322win@gmail.com>
2024-07-22 10:02:53 +0800

    generate card > preview

--- js/script.js
+++ js/script.js
@@ -2,15 +2,43 @@ let characterCount = 0;
 function generateCardHTML(character) {
   //console.log(character);
   characterCount++;
-  const characterId = `character-${characterCount}`;
+  const characterId = `${characterCount}`;
   return `
-    <div class="product" data-character-id="${characterId}">
+    <div class="product" data-name="p-${characterId}">
       <img src="${character.image}" alt="${character.name}">
       <h3>${character.name}</h3>
       <div class="price">$${character.price}</div>
     </div>
   `;
 }
+
+const characterCards = document.querySelector(".products-container");
+let characters = {};
+fetch('data.json').then(response => response.json()).then(data => {
+  characters = data;
+
+  characters.forEach(character => {
+    const cardOPDT = generateCardHTML(character);
+    console.log(cardOPDT);
+    characterCards.innerHTML += cardOPDT;
+    // characterCards.appendChild(cardOPDT);
+  });
+  let preveiwContainer = document.querySelector('.products-preview');
+  let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+  document.querySelectorAll('.products-container .product').forEach(product =>{
+    product.onclick = () =>{
+      preveiwContainer.style.display = 'flex';
+      let name = product.getAttribute('data-name');
+      previewBox.forEach(preview =>{
+        let target = preview.getAttribute('data-target');
+        if(name == target){
+          preview.classList.add('active');
+        }
+      });
+    };
+  });
+})
 
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
@@ -28,6 +56,21 @@ document.querySelectorAll('.products-container .product').forEach(product =>{
   };
 });
 
+// characterCards.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('product')) {
+//     const product = event.target;
+//     preveiwContainer.style.display = 'flex';
+//     let name = product.getAttribute('data-name');
+//     previewBox.forEach(preview => {
+//       let target = preview.getAttribute('data-target');
+//       if (name == target) {
+//         preview.classList.add('active');
+//       }
+//     });
+//   }
+// });
+
+
 previewBox.forEach(close =>{
   close.querySelector('.fa-times').onclick = () =>{
     close.classList.remove('active');
@@ -35,13 +78,3 @@ previewBox.forEach(close =>{
   };
 });
 
-const characterCards = document.querySelector(".products-container");
-let characters = {};
-fetch('data.json').then(response => response.json()).then(data => {
-  characters = data;
-
-  characters.forEach(character => {
-    // const cardOPDT = generateCardHTML(character);
-    characterCards.innerHTML += cardOPDT;
-  })
-})
