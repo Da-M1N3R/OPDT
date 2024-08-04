1fd5e6aaa3740d6028b796d120f9d60a6650ce7d
Edgar <uranium322win@gmail.com>
2024-08-04 21:27:14 +0800

    next step prevBtn & nextBtn function

--- js/script.js
+++ js/script.js
@@ -7,30 +7,46 @@ function generateCardHTML(character) {
       <img src="${character.image}" alt="${character.name}">
       <h3>${character.name}</h3>
       <div class="price">$${character.price}</div>
-      <p>Sorry</p>
     </div>
   `;
 }
 let extraImgCounter = 0;
 function generatePreviewCharInfo(character, extraImgCounter=0) {
+  let extraImgPg = extraImgCounter + 1;
+  let extraImgAllPg = character.extras.length;
   const characterId = `${characterCount}`;
+  console.log("CharProduct -", characterCount.toString(),": " , character.name);
   return `
     <div class="preview" data-target="p-${characterId}">
       <i class="fas fa-times"></i>
       <div class="previewCard">
-        <img src="${character.image}" alt="${character.name}">
-        <h3>${character.name}</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.</p>
-        <div class="price">$${character.price}</div>
-        <div class="buttons">
-          <a href="#" class="buy">buy now</a>
-          <a href="#" class="cart">add to team</a>
+
+        <div class="name-price">
+          <h3>${character.name}</h3>
+          <div class="buttons">
+            <a href="#" class="buy">Buy</a>
+          </div>
         </div>
+
+        <div class="img-priceContainer">
+          <img src="${character.image}" alt="${character.name}">
+          <div class="price">$${character.price}</div>
+        </div>
+
+        <p>
+          Saint Figarland Garling is a World Noble of the Figarland Family and the Supreme Commander (最高司令官, Saikō Shireikan?) of the God's Knights, of whom he is the first to be introduced. He was formerly active as a "champion" on God Valley.[1]
+        </p>
       </div>
+
       <div class="extraImg">
-        <i class="fas fa-chevron-left"></i>
-        <img src="${character.extras[extraImgCounter]}" alt="${character.name}">
-        <i class="fas fa-chevron-right"></i>
+        <i class="fas fa-chevron-left" id="prevBtnID-${characterId}"></i>
+
+        <div class="extraImgBox">
+          <img src="${character.extras[extraImgCounter]}" alt="${character.name}">
+          <p>(${extraImgPg}/${extraImgAllPg})</p>
+        </div>
+
+        <i class="fas fa-chevron-right" id="nextBtnID-${characterId}"></i>
       </div>
     </div>
   `;
@@ -44,6 +60,32 @@ function closePreview(event) {
     previewContainer.style.display = 'none';
   }
 }
+
+// ...
+
+function printCurrentPreviewProduct(product) {
+  return product;
+}
+
+function testLeft(product) {
+  let name = product.getAttribute('data-target');
+  console.log("Billy choose = ", name);
+}
+function tester() {
+  console.log("Billy's here...");
+}
+
+
+function handleRightClick() {
+  console.log("nextBtn clicked")
+
+  extraImgCounter++; // increment counter
+  // Update img src attribute
+  const img = document.querySelector('.preview .extraImg img');
+  img.src = character.extras[extraImgCounter];
+  return img.src;
+}
+// ...
 
 let i = 0;
 function prevExImg() {
@@ -59,16 +101,47 @@ function nextExImg() {
 
 // Product Image const
 const characterCards = document.querySelector(".products-container");
-let characters = {};
+
 
 // Preview Image const
 const previewContainer = document.querySelector('.products-preview');
 
 let charExtraLength = 0;
-const body = document.querySelector(".body");
-console.log(body);
 
 'use strict';
+
+function getActiveChar(characters) {
+  characters.forEach(character => {
+    if (document.querySelectorAll('.active') == true) {
+      x = document.querySelectorAll('.active');
+      console.log("X is here", x);
+    }
+  })
+}
+function getJust(n) {
+  extrasNum = characters[n].extras
+  console.log("Just selected products >>>", extrasNum);
+  // generatePreviewCharInfo(characters[n]);
+  console.log("Billy signing off >>> ", extrasNum[1]);
+}
+function target_StringSlice(n) {
+  numberPart = n.substring(2);
+  num = parseInt(numberPart);
+  return num
+}
+
+function handleLeftClick() {
+  console.log("prevBtn clicked")
+  console.log(extraImgCounter);
+
+  extraImgCounter--; // decrement counter
+  // Update img src attribute
+  generatePreviewCharInfo(characters[target_StringSlice[n]], extraImgCounter) 
+  const img = document.querySelector('.preview .extraImg img');
+}
+
+let characters;
+
 
 fetch('data.json').then(response => response.json()).then(data => {
   characters = data;
@@ -83,25 +156,85 @@ fetch('data.json').then(response => response.json()).then(data => {
   });
 
   const previewBox = previewContainer.querySelectorAll('.preview');
+  
   document.querySelectorAll('.products-container .product').forEach(product =>{
     product.onclick = () =>{
+      
       previewContainer.style.display = 'flex';
       let name = product.getAttribute('data-name');
+
       previewBox.forEach(preview =>{
+
         let target = preview.getAttribute('data-target');
-        console.log(target);
-        console.log("2nd hook, name = ", name, "& target = ", target);
+        // console.log(target);
+        // console.log("2nd hook, name = ", name, "& target = ", target);
+
         if(name == target){
-          console.log("name == target >>> success!!!")
+
+          console.log("name=", name, "& target=", target);
+          // console.log(typeof target);
+          n = target_StringSlice(target) - 1;
+          console.log("n = ", n);
+          getJust(n);
+          let previewResult = printCurrentPreviewProduct(preview);
+          // console.log(previewResult);
+          // testLeft(previewResult);
+          // console.log("name == target >>> success!!!")
           preview.classList.add('active');
+          console.log(preview);
+          
+          console.log("Billy is here with: ", target);
+          // console.log(leftChevron);
+          // console.log(rightChevron);
+          
+
+          console.log("billy closed the door")
+
+          const img = preview.querySelector('.preview .extraImg img');
+          // console.log(img.src)
+
+          let idName = 'prevBtnID-' + (n + 1).toString()
+          const leftBtn = document.getElementById(idName);
+          console.log("Daki", idName);
+          // leftBtn.addEventListener('click', event => {
+          //   console.log("billy, we are clicking LEFT.");
+          // })
+          leftBtn.addEventListener('click', event => {
+            generatePreviewCharInfo(characters[0], extraImgCounter=1);
+          })
+          
+          // leftBtn.id = 'prevBtn';
+
+          // console.log("Doma", leftBtn);
         }
+
+        // if(true) {
+        //   console.log("billy showing: ", preview)
+        //   const previewOn = preview.querySelector('.active');
+        //   console.log(previewOn);
+        // }
+        
       });
     };
   });
+
   previewBox.forEach(close =>{
     close.querySelector('.fa-times').onclick = () =>{
       close.classList.remove('active');
       previewContainer.style.display = 'none';
     };
   });
+  // //new
+  // const leftChevron = document.getElementById('prevBtn');
+  // const rightChevron = document.getElementById('nextBtn');
+  // console.log(leftChevron);
+  // console.log("Billy bob")
+  // leftChevron.addEventListener('click', event => {
+  //   console.log("Say cheese");
+  // });
 })
+const nothingBtn = document.getElementById('nothing');
+nothingBtn.addEventListener('click', event => {
+  console.log("billy is back, & he brought Alicia.");
+})
+
